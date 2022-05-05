f1ac9f8850cf421d3a65d2233617b5bf28418df6
GGolfz <wisarutgolf11@gmail.com>
2022-05-05 21:30:38 +0700

    add: Todo Class :sparkles:

---
+++ src/main/java/com/example/todoapplication/domain/Todo.java
@@ -0,0 +1,30 @@
+package com.example.todoapplication.domain;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter @Setter
+@Entity
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long Id;
+
+    private String title;
+    private String description;
+    private boolean finish;
+
+    protected Todo() {}
+
+    public Todo(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
+
+}
